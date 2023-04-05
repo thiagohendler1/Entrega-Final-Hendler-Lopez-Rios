108e02f8a6b8a02c5a42054c4c0b51ba3d6de2b8
Felipe Lopez Rios <felipetomaslopezrios@gmail.com>
2023-04-05 20:32:18 -0300

    Creada la parte de CREAR del CRUD publicaciones

--- project/gestion_publicaciones/urls.py
+++ project/gestion_publicaciones/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from gestion_publicaciones.views import HomeView, TiendaView, GenerosView, AboutView
+from gestion_publicaciones.views import HomeView, TiendaView, GenerosView, AboutView, PublicacionCreateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='inicio'),
     path('shop/', TiendaView, name='tienda'),
+    path('shop/create_publication/', PublicacionCreateView.as_view(), name='crear_publicacion'),
     path('genres/', GenerosView, name='generos'),
     path('about/', AboutView.as_view(), name='sobre_nosotros'),
 ]
