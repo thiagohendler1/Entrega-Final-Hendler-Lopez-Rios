1387bc5a19b527bde6c7f3601b505e76ce98ca0b
Felipe Lopez Rios <felipetomaslopezrios@gmail.com>
2023-04-06 14:51:59 -0300

    Actualizacion plantilla 'inicio.html'

--- project/gestion_publicaciones/urls.py
+++ project/gestion_publicaciones/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', HomeView.as_view(), name='inicio'),
     path('shop/', TiendaView, name='tienda'),
     path('shop/create_publication/', PublicacionCreateView.as_view(), name='crear_publicacion'),
-    path('shop/publication_detail/<pk>/', PublicacionDetailView.as_view(), name='detalle_publicacion'),
-    path('shop/update_publication/<pk>/', PublicacionUpdateView.as_view(), name='editar_publicacion'),
+    path('shop/publication_detail/<int:pk>/', PublicacionDetailView.as_view(), name='detalle_publicacion'),
+    path('shop/update_publication/<int:pk>/', PublicacionUpdateView.as_view(), name='editar_publicacion'),
     path('genres/', GenerosView, name='generos'),
     path('about/', AboutView.as_view(), name='sobre_nosotros'),
 ]
--- project/gestion_publicaciones/views.py
+++ project/gestion_publicaciones/views.py
@@ -19,10 +19,11 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         # Guardado de las publicaciones mas recientes
         objetos = Publicacion.objects.all().order_by('-id')[:3]
-        objeto_uno = objetos[0]
-        objeto_dos = objetos[1]
-        objeto_tres = objetos[2]
-        context.update({'objeto_uno': objeto_uno, 'objeto_dos': objeto_dos, 'objeto_tres': objeto_tres})
+        if objetos:
+            objeto_uno = objetos[0] if len(objetos) >= 1 else None
+            objeto_dos = objetos[1] if len(objetos) >= 2 else None
+            objeto_tres = objetos[2] if len(objetos) >= 3 else None
+            context.update({'objeto_uno': objeto_uno, 'objeto_dos': objeto_dos, 'objeto_tres': objeto_tres, 'objetos': objetos})
         return context
 
 # LOGIN REQUIRED
